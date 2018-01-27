cf4890c906dcb38a5e2496796287cbe4bb035905
lokimuj <lokimuj9@gmail.com>
2018-01-27 13:29:44 -0500

    constants added to chunk

--- src/model/Chunk.java
+++ src/model/Chunk.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Chunk {
+	public static final double length = 1000;
+	public static final double width = 1000; //10,000km
 
 	Map<Entity, Equation> entityEquationMap = new HashMap<>();
 	private int x;
