2726e27635a7225dfde7bff778454d4554fb8d94
lokimuj <lokimuj9@gmail.com>
2018-01-27 14:09:56 -0500

    Changed equation for lambda use

--- src/model/Equation.java
+++ src/model/Equation.java
@@ -3,22 +3,27 @@ package model;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class Equation {
+public interface Equation {
 
-	private Entity entity;
-	private Method equation;
-
-	public Equation(Method equation){
-		this.equation = equation;
-	}
-
-	public void setEntity(Entity entity){
-		this.entity = entity;
-	}
-
-	public Vector computeFunction(Vector vector) throws InvocationTargetException, IllegalAccessException {
-		return (Vector) equation.invoke(vector);
-	}
+	Vector evaluate(Vector v);
 
 
+//	private Entity entity;
+//	private Method equation;
+//
+//	public Equation(Method equation){
+//		this.equation = equation;
+//	}
+//
+//	public void setEntity(Entity entity){
+//		this.entity = entity;
+//	}
+//
+//	public Vector computeFunction(Vector vector) throws InvocationTargetException, IllegalAccessException {
+//		return (Vector) equation.invoke(vector);
+//	}
+//
+//	public static void main(String[] args){
+//		Equation eq = new Equation((Vector v) -> {return (v.getX()+v.getY());} );
+//	}
 }
