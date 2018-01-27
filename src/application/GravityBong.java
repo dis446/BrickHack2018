4c5ac9e6fa555f975b9b0fd0d96fc0ac224b9ee6
Disney <dis446@yahoo.com>
2018-01-27 13:28:36 -0500

    Start of High level World and Main Application coding

--- src/application/GravityBong.java
+++ src/application/GravityBong.java
@@ -1,9 +1,24 @@
 package application;
 
+import model.Chunk;
+import model.World;
+
 public class GravityBong {
+
+	World world;
+
+	public GravityBong(int length, int column) {
+		this.world = new World(length, column);
+	}
+
 	public static void main(String[] args){
 		if (args.length != 2){
-			System.out.println("Error. Usage: java GravityBong length column");
+			System.out.println("Error. Usage: java GravityBong length width");
+			System.exit(0);
 		}
+		int length = Integer.parseInt(args[0]);
+		int column = Integer.parseInt(args[1]);
+		GravityBong gravityBong = new GravityBong(length, column);
 	}
+
 }
--- src/model/World.java
+++ src/model/World.java
@@ -3,4 +3,16 @@ package model;
 
 public class World {
 
+	Chunk[][] chunks;
+
+	public World(int length, int width){
+		this.chunks = new Chunk[length][width];
+		for (int i = 0; i < length; i++){
+			for (int j = 0; j < width; j++){
+				Chunk chunk = new Chunk(i, j);
+				chunks[i][j] = chunk;
+			}
+		}
+	}
+
 }
