8128544f1fc62e5ac10e382554876217ba5d180d
Disney <dis446@yahoo.com>
2018-01-29 16:36:36 -0500

    JavaDoc Entity and Chunk

--- src/model/Chunk.java
+++ src/model/Chunk.java
@@ -25,29 +25,56 @@ public class Chunk extends Observable{
 		this.world = world;
 	}
 
+	/**
+	 * Add a given entity to this chunk.
+	 * @param e
+	 */
 	public void addEntity(Entity e){
 		entities.add(e);
 		if(e instanceof Planet){
 			planetInfluences.add((Planet) e);
 		}
 	}
+
+	/**
+	 * Add a given planet to the list of influencers on this chunk.
+	 * @param e
+	 */
 	public void addInfluence(Planet e){
 		planetInfluences.add(e);
 	}
 
+	/**
+	 * Remove a given entity from this chunk
+	 * @param e
+	 */
 	public void removeEntity(Entity e){
 		entities.remove(e);
 	}
 
+	/**
+	 * Remove a given planet from the list of influencers on this chunk.
+	 * @param p
+	 */
 	public void removeInfluence(Planet p){
 		planetInfluences.remove(p);
 	}
 
+	/**
+	 * Boolean check to see if a given entity is within the bounds of this chunk.
+	 * @param e
+	 * @return
+	 */
 	public boolean inBound(Entity e){
 		Vector pos = e.getPosition();
 		return (pos.getX()>= xPos && pos.getY()>= yPos && pos.getX()<xPos+side && pos.getY()<yPos+side);
 	}
 
+	/**
+	 * Apply the changes to this chunk(and all entities contained within) based on the difference between two given times.
+	 * @param baseTime
+	 * @param newTime
+	 */
 	public void step(long baseTime, long newTime){
 		Set<Entity> copy = new HashSet<>(entities);
 		for(Entity e: copy) {
@@ -68,6 +95,15 @@ public class Chunk extends Observable{
 		}
 	}
 
+	/**
+	 * Return a string of the format: "[Vector
+	 * 	Entity 1
+	 * 	Entity 2
+	 * 	Entity 3
+	 * 	...
+	 * 	]
+	 * @return
+	 */
 	@Override
 	public String toString() {
 		String out = "[ "+ new Vector(xPos,yPos)+"\n";
--- src/model/entities/Entity.java
+++ src/model/entities/Entity.java
@@ -5,6 +5,9 @@ import model.Vector;
 
 public abstract class Entity {
 
+	/**
+	 * Stores the subtype of entity that this instance is of.
+	 */
 	EntityType type = EntityType.NONE;
 	Vector position;
 	Vector velocity;
@@ -21,14 +24,28 @@ public abstract class Entity {
 		this.color = color;
 	}
 
+	/**
+	 * Creates an entity with zero initial velocity
+	 * @param position
+	 * @param name
+	 * @param color
+	 */
 	public Entity(Vector position,String name, Texture color){
 		this(position,new Vector(0,0),name,color);
 	}
 
+	/**
+	 * Getter for the name.
+	 * @return string
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * ToString in the form: "[type: Position: pos Velocity: vel Acceleration: acc]"
+	 * @return
+	 */
 	@Override
 	public String toString() {
 		String out = "["+type.getName()+": "+name+" Position: "+position+" Velocity: "+velocity+" Acceleration: "
@@ -36,31 +53,63 @@ public abstract class Entity {
 		return out;
 	}
 
+	/**
+	 * Getter for the type.
+	 * @return
+	 */
 	public EntityType getType() {
 		return type;
 	}
 
+	/**
+	 * Get the position vector.
+	 * @return
+	 */
 	public Vector getPosition() {
 		return position;
 	}
 
+	/**
+	 * Getter for the velocity vector.
+	 * @return
+	 */
 	public Vector getVelocity() {
 		return velocity;
 	}
 
+	/**
+	 * Getter for the color.
+	 * @return
+	 */
 	public Texture getColor() { return color; }
 
+	/**
+	 * Getter for canMove
+	 * @return
+	 */
 	public boolean canMove(){return true;}
 
+	/**
+	 * Setter for the acceleration vector.
+	 * @param acc
+	 */
 	public void applyAcc(Vector acc){
 		curAcceleration = acc;
 	}
 
+	/**
+	 * Apply the changes that a difference in time causes on this entities velocity and position.
+	 * @param timeStep
+	 */
 	public void step(double timeStep){
 		velocity.change(Vector.scalar(timeStep,curAcceleration));
 		position.change(Vector.scalar(timeStep, velocity));
 	}
 
+	/**
+	 * Output a String in the format that can be stored into a file.
+	 * @return
+	 */
 	public abstract String toFileFormat();
 
 }
